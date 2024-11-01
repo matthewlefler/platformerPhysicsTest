05a58abe936a7bb2d78bbbf3a09eecb2545b639d
matthewlefler <matthewlefler1@gmail.com>
2024-10-31 22:28:44 -0400

    huh

--- platformerPhysicsTest/src/main.rs
+++ platformerPhysicsTest/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
-}
+    App::new()
+      .add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+          mode: WindowMode::Fullscreen,
+          ..default()
+        }),
+        ..default()
+      }))
+      .add_startup_system(setup_system)
+      .add_system(hello_world_system)
+      .run();
+  }
+  
+  fn setup_system() {
+    println!("Welcome to ferris spin!");
+  }
+  
+  fn hello_world_system() {
+    println!("hello world");
+  }
