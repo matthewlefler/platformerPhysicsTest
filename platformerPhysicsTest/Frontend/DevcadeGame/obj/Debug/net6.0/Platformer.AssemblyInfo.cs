fd5a81b0841725f445aba1eea29a97892c251ba3
matthewlefler <matthewlefler1@gmail.com>
2024-11-11 16:16:30 -0500

    why

--- platformerPhysicsTest/Frontend/DevcadeGame/obj/Debug/net6.0/Platformer.AssemblyInfo.cs
+++ platformerPhysicsTest/Frontend/DevcadeGame/obj/Debug/net6.0/Platformer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Platformer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d17069cc85db3b4966b83c011582c23a422c740b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f0fb800f79d131c35c7be916ad7d916ea1c0e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Platformer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Platformer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
